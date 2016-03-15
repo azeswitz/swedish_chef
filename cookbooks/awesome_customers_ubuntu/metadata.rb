7cd9675cd7424789bf389bf15777d4aa795c1c66
Andrew Zeswitz <azeswitz@gmail.com>
2016-03-15 17:10:57 -0400

    Debug node deployment - issue was the databag was not created in Chef server

--- cookbooks/awesome_customers_ubuntu/metadata.rb
+++ cookbooks/awesome_customers_ubuntu/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'andrew.zeswitz@icfi.com'
 license 'all_rights'
 description 'Installs/Configures awesome_customers_ubuntu'
 long_description 'Installs/Configures awesome_customers_ubuntu'
-version '0.1.3'
+version '0.1.4'
 
 depends 'apt', '~> 3.0.0'
 depends 'firewall', '~> 2.5.0'
